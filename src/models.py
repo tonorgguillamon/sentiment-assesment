f988749beea101255f8c8d7fe694a84506e92ea1
Tono <tonorgguillamon@gmail.com>
2025-03-23 10:38:28 +0100

    vader and textblob models added

--- src/models.py
+++ src/models.py
@@ -1,5 +1,8 @@
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
+from textblob import TextBlob
 
 class DataModel():
     """
@@ -18,6 +21,19 @@ class BERT():
     https://huggingface.co/datasets/gxb912/large-twitter-tweets-sentiment
     https://huggingface.co/datasets/cardiffnlp/tweet_eval
     https://huggingface.co/datasets/mteb/tweet_sentiment_extraction
+
+    Self-attention Mechanism:
+        - relationships between words
+        - process all words at one (parallel processing)
+    Multi-head attention: grammar and meaning.
+    Positional Encoding: order in the sentence matters.
+
+    Insights:
+        1. Tokenizes text.
+        2. Passes it through multiple transformer layers.
+        3. Understands relationships between words.
+        4. Outputs "logits" for different sentiment classes.
+        5. Final softmax layer to convert logits into probabilities.
     """
     def __init__(self, modelName: str):
         self.tokenizer = BertTokenizer.from_pretrained(modelName)
@@ -40,6 +56,38 @@ class BERT():
 
         return {"sentiment": sentiment, "scores": dict(zip(labels, scores))}
     
+class VADER(): # Valence Aware Dictionary and sEntiment Reasoner
+    def __init__(self):
+        self.sia = SentimentIntensityAnalyzer()
+
+    def getSentiment(self, text: str):
+        scores = self.sia.polarity_scores(text)
+        return scores # (neg, pos, compound): see compound if > 0.05 positive sentiment, < -0.05 negative sentiment, between -0.05 and 0.05 neutral. Goes from -1 to 1
+
+class TEXTBLOB():
+    """
+    TextBlob provides a simple way to analyze the sentiment of text. It returns two main values:
+    - Polarity: A value between -1.0 (negative) and 1.0 (positive).
+    - Subjectivity: A value between 0.0 (objective - fact-based) and 1.0 (subjective - opinion-based).
+
+    It analizes the text sentence by sentence.
+    iterate over blob.sentences
+    """
+    def getSentiment(self, text: str):
+        blob = TextBlob(text)
+        scores = blob.sentiment
+        return {"polarity": scores.polarity, "subjectivity": scores.subjectivity}
+
+class DEEPMOJI():
+    pass
+    
 if __name__ == "__main__":
     bertModel = BERT("nlptown/bert-base-multilingual-uncased-sentiment")
-    print(bertModel.getSentiment("I am not sure"))
+    vaderModel = VADER()
+    textblobModel = TEXTBLOB()
+
+    text = "I am not sure"
+
+    print(f"Bert Model: {bertModel.getSentiment(text)}")
+    print(f"Vader Model: {vaderModel.getSentiment(text)}")
+    print(f"TextBlob Model: {textblobModel.getSentiment(text)}")
